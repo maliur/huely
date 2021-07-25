fd3af68850ace934dff7d3d27823e54db94ae2cb
Maliur <maliur@protonmail.com>
2021-07-25 15:57:22 +0200

    Refactor code

--- main.go
+++ main.go
@@ -3,14 +3,94 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/rivo/tview"
 	"net/http"
 	"os"
+	"time"
 )
+
+var ColorLoop string = "colorloop"
+
+type Group struct {
+	ID     string
+	Name   string   `json:"name"`
+	Type   string   `json:"type"`
+	Lights []string `json:"lights"`
+}
 
 type Light struct {
 	ID   string
 	Name string `json:"name"`
+}
+
+type Config struct {
+	ApiKey string
+	HubIP  string
+}
+
+type Hue struct {
+	client *http.Client
+	config Config
+}
+
+func NewHue(config Config) *Hue {
+	return &Hue{
+		client: &http.Client{Timeout: time.Second * 60},
+		config: config,
+	}
+}
+
+func (h *Hue) FetchLights() ([]Light, error) {
+	resp, err := h.client.Get(fmt.Sprintf("http://%s/api/%s/lights", h.config.HubIP, h.config.ApiKey))
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	var body map[string]Light
+
+	err = json.NewDecoder(resp.Body).Decode(&body)
+	if err != nil {
+		return nil, err
+	}
+
+	var lights []Light
+	for k, v := range body {
+		v.ID = k
+		lights = append(lights, v)
+	}
+
+	return lights, nil
+}
+
+func (h *Hue) FetchGroups() ([]Group, error) {
+	resp, err := h.client.Get(fmt.Sprintf("http://%s/api/%s/groups", h.config.HubIP, h.config.ApiKey))
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	var body map[string]Group
+
+	err = json.NewDecoder(resp.Body).Decode(&body)
+	if err != nil {
+		return nil, err
+	}
+
+	var groups []Group
+	for k, v := range body {
+		v.ID = k
+		groups = append(groups, v)
+	}
+
+	return groups, nil
+}
+
+func prettyPrint(data interface{}) {
+	j, _ := json.MarshalIndent(data, "", "\t")
+
+	fmt.Println(string(j))
 }
 
 func main() {
@@ -22,37 +102,32 @@ func main() {
 		return
 	}
 
-	resp, err := http.Get(fmt.Sprintf("http://%s/api/%s/lights", hubIP, apiKey))
-	if err != nil {
-		fmt.Printf("could not make request: %v", err)
+	if len(hubIP) == 0 {
+		fmt.Println("HUE_HUB_IP not provided")
 		return
 	}
 
-	defer resp.Body.Close()
+	config := Config{
+		ApiKey: apiKey,
+		HubIP:  hubIP,
+	}
 
-	var body map[string]Light
+	hue := NewHue(config)
 
-	err = json.NewDecoder(resp.Body).Decode(&body)
+	_, err := hue.FetchLights()
 	if err != nil {
-		fmt.Printf("could decode json body: %v", err)
+		fmt.Printf("could not fetch lights: %v", err)
 		return
 	}
 
-	var lights []Light
-	for k, v := range body {
-		v.ID = k
-		lights = append(lights, v)
+	groups, err := hue.FetchGroups()
+	if err != nil {
+		fmt.Printf("could not fetch groups: %v", err)
+		return
 	}
 
-	fmt.Println(lights)
+	// prettyPrint(lights)
+	prettyPrint(groups)
 
-	j, _ := json.MarshalIndent(lights, "", "\t")
-
-	app := tview.NewApplication()
-	flex := tview.NewFlex().
-		AddItem(tview.NewBox().SetBorder(true).SetTitle("Lights"), 0, 1, false).
-		AddItem(tview.NewTextView().SetText(string(j)), 0, 1, false)
-	if err := app.SetRoot(flex, true).EnableMouse(true).Run(); err != nil {
-		panic(err)
-	}
+	// fmt.Println(groups)
 }
